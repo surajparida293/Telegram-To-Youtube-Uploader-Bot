1c8cd0d96928b4640e19f8ece0d615a91482545a
surajparida293 <surajparida293@gmail.com>
2024-08-03 15:48:17 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,27 +2,27 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7250372713:AAGpB9aqCUk7TuA6DhNKsOzhh8P6Z7Iaz2c"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "surajparida_bot"
 
-    API_ID = ""
+    API_ID = "29684646"
 
-    API_HASH = ""
+    API_HASH = "265e4dd164cb7770dd25c8262c0e1bb5"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "9741184869-aj72g6rp0nsf6n53dmau9ut0fbam7erq.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5948639081]
 
     VIDEO_DESCRIPTION = ""
 
     VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "hi"
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "last"
     
     DEBUG = bool()
 
